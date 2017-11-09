5173559c2f8aaf49d15432fe78ee5010ade988f0
Yoram Michaeli <yorammi@tikalk.com>
2017-11-09 03:49:55 +0200

    Add tplGenericPythonRunner

---
+++ src/tpl/python/runners/tplGenericPythonRunner.groovy
@@ -0,0 +1,31 @@
+package tpl.python.runners;
+
+import tpl.python.baseGenericPythonRunner
+
+class tplGenericPythonRunner extends tplBaseGenericPythonRunner {
+
+    tplGenericPythonRunner(script) {
+        super(script)
+    }
+
+
+    @Override
+    void initParams()
+    {
+        gitCredentialsId = script.env.GIT_TOKEN
+        gitRepoUrl = script.env.REPOSITORY
+        gitBranch = script.env.BRANCH
+        pythonRequirements = script.env.REQUIREMENTS_FILE
+        pythonScript = script.env.PYTHON_SCRIPT_FILE
+        groovySetupScript = script.params.GROOVY_SCRIPT
+    }
+
+    @Override
+    void commonRequirements(){
+        script.echo "Install common requirements"
+        script.sh '''
+            pip install requests
+        '''
+    }
+
+}
