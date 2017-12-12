e4bf4fac090adb47b6b53a7560614b90e91e3877
Yoram Michaeli <yorammi@tikalk.com>
2017-12-12 15:42:57 +0200

    Add tplGetTestSummary function

---
+++ vars/tplGetTestSummary.groovy
@@ -0,0 +1,22 @@
+import hudson.tasks.test.AbstractTestResultAction
+
+def call() {
+
+    def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+    def summary = ""
+
+    if (testResultAction != null) {
+        def total = testResultAction.getTotalCount()
+        def failed = testResultAction.getFailCount()
+        def skipped = testResultAction.getSkipCount()
+
+        summary = "[Test results] "
+        summary = summary + ("Passed: " + (total - failed - skipped))
+        summary = summary + (", Failed: " + failed)
+        summary = summary + (", Skipped: " + skipped)
+    } else {
+        summary = "[Tests result] No tests found"
+    }
+    println("${summary}")
+    return summary
+}
