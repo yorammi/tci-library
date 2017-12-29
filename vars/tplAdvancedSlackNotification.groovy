b1eb66be484932c2d8eb77e992feacf70dc9cf58
Yoram Michaeli <yorammi@tikalk.com>
2017-12-29 05:29:35 +0200

    Add try-catch to Slack step

--- vars/tplAdvancedSlackNotification.groovy
+++ vars/tplAdvancedSlackNotification.groovy
@@ -1,45 +1,52 @@
-def call(String buildStatus, String channel, String additionalMessageText = "") {
+def call(String buildStatus, String channel, String additionalMessageText = "")
+{
+    try
+    {
+        buildStatus = buildStatus ?: 'SUCCESS'
 
-    buildStatus = buildStatus ?: 'SUCCESS'
+        // Default values
+        def colorName = 'RED'
+        def colorCode = '#FF0000'
+        def icon = ':tired_face: '
 
-    // Default values
-    def colorName = 'RED'
-    def colorCode = '#FF0000'
-    def icon = ':tired_face: '
+        // Override default values based on build status
+        if (buildStatus == 'STARTED')
+        {
+            color = 'YELLOW'
+            colorCode = '#FFFF00'
+            icon = ''
+        }
+        else if (buildStatus == 'SUCCESS')
+        {
+            color = 'GREEN'
+            colorCode = '#00CF00'
+            icon = ':+1: '
+        }
+        else if (buildStatus == 'FAILURE')
+        {
+            color = 'RED'
+            colorCode = '#FF0000'
+            icon = ':scream: '
+        }
+        else if (buildStatus == 'ABORTED')
+        {
+            color = 'GRAY'
+            colorCode = '#AAAAAA'
+            icon = ''
+        }
+        else
+        {
+            color = 'PINK'
+            colorCode = '#FFCCCC'
+            icon = ''
+        }
 
-    // Override default values based on build status
-    if (buildStatus == 'STARTED')
-    {
-        color = 'YELLOW'
-        colorCode = '#FFFF00'
-        icon = ''
+        def summary = "${icon}*BUILD ${buildStatus}*\n\n[Job] *${env.JOB_NAME} #${env.BUILD_NUMBER}*\n[Name] *${currentBuild.displayName}*\n[Console] ${env.BUILD_URL}/consoleFull\n${additionalMessageText}"
+        slackSend (color: colorCode, message: summary, channel: "#${channel}" )
     }
-    else if (buildStatus == 'SUCCESS')
+    catch(error)
     {
-        color = 'GREEN'
-        colorCode = '#00CF00'
-        icon = ':+1: '
-    }
-    else if (buildStatus == 'FAILURE')
-    {
-        color = 'RED'
-        colorCode = '#FF0000'
-        icon = ':scream: '
-    }
-    else if (buildStatus == 'ABORTED')
-    {
-        color = 'GRAY'
-        colorCode = '#AAAAAA'
-        icon = ''
-    }
-    else
-    {
-        color = 'PINK'
-        colorCode = '#FFCCCC'
-        icon = ''
+        echo error.message
     }
 
-    def summary = "${icon}*BUILD ${buildStatus}*\n\n[Job] *${env.JOB_NAME} #${env.BUILD_NUMBER}*\n[Name] *${currentBuild.displayName}*\n[Console] ${env.BUILD_URL}/consoleFull\n${additionalMessageText}"
-    echo summary
-    slackSend (color: colorCode, message: summary, channel: "#${channel}" )
 }
