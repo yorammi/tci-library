8eaf39a9861287bdb398d3cd89cfc21eacbf1de3
yorammi <yorammi@tikalk.com>
2018-09-26 18:18:36 +0300

    add helm installation

--- src/tpl/services/Deployer.groovy
+++ src/tpl/services/Deployer.groovy
@@ -231,6 +231,7 @@ class Deployer implements Serializable{
         script.sh "curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh"
         script.sh "sed -i 's|sudo| |g' get_helm.sh"
         script.sh "chmod 700 get_helm.sh"
+        script.sh "cat get_helm.sh"
         script.withEnv(["HELM_INSTALL_DIR=/home/jenkins/"]) {
             script.sh "./get_helm.sh"
         }
