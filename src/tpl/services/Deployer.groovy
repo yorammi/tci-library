6a40b9359953eeb52f4f97aad17dda63b3efc9d9
Itai <itai@tikalk.com>
2018-09-26 12:04:33 +0300

    Add node js

--- src/tpl/services/Deployer.groovy
+++ src/tpl/services/Deployer.groovy
@@ -204,7 +204,7 @@ class Deployer implements Serializable{
     }
     void helmInit(){
         logger.info "in init"
-        script.podTemplate(label: label, containers: [
+        script.podTemplate(label: 'label', containers: [
                 script.containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.8', command: 'cat', ttyEnabled: true),
                 script.containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:latest', command: 'cat', ttyEnabled: true)
         ],
