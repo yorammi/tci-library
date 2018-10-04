cac5dc0f225012b6f91c112b447e9f22372f5352
yorammi <yorammi@tikalk.com>
2018-10-04 11:54:05 +0300

    staging helm deployment

--- vars/tplRunStage.groovy
+++ vars/tplRunStage.groovy
@@ -1,8 +1,8 @@
 def call(String name, Closure stage) {
-    if (currentBuildResult in ['SUCCESS', null]) {
+    if (currentBuild.currentResult in ['SUCCESS', null]) {
         echo "Start stage $name"
         stage(name, stage)
-        echo "End stage $name with result ${currentBuildResult ?: 'SUCCESS'}"
+        echo "End stage $name with result ${currentBuild.currentResult ?: 'SUCCESS'}"
     } else {
         stage(name) {
             echo "Build is unstable, skipping stage $name"
