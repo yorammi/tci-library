7e5713587e08188028633fee3d046a55c28cb0f7
yorammi <yorammi@tikalk.com>
2018-10-04 15:20:06 +0300

    push to s3

--- src/tpl/services/Deployer.groovy
+++ src/tpl/services/Deployer.groovy
@@ -71,7 +71,7 @@ class Deployer implements Serializable{
             //pushCode()
             script.sh "helm package ."
             script.withEnv(["AWS_REGION=us-east-1"]) {
-                script.sh "helm s3 push --force ./${it}-${newVersion}.tgz ants"
+                script.sh "helm s3 push --force ./${it}-${newVersion}.tgz"
             }
         }
         updateHelmUmbrella(it)
