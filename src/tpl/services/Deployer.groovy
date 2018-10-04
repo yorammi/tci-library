85b9f3980126a13cff7fedf5a6710d8f46ea718c
yorammi <yorammi@tikalk.com>
2018-10-04 13:09:48 +0300

    no need to install kubectl

--- src/tpl/services/Deployer.groovy
+++ src/tpl/services/Deployer.groovy
@@ -176,14 +176,14 @@ class Deployer implements Serializable{
     void helmInit(){
         logger.info "in init"
         script.dir("${script.env.WORKSPACE}"){
-             installKubectl()
+             //installKubectl()
              script.withEnv(["HELM_HOST=AAA", "AWS_REGION=eu-west-1"]) {
                    // script.withCredentials([script.file(credentialsId: 'kube-config', variable: 'FILE')]) {
                  script.withCredentials([script.kubeconfigContent(credentialsId: 'kube-config', variable: 'KUBECONFIG_CONTENT')]){
                         script.sh "mkdir -p ~/.kube"
                         script.sh "echo \"${script.env.KUBECONFIG_CONTENT}\" > /home/jenkins/.kube/config"
-                        script.sh "~/kubectl config  current-context"
-                        script.sh "~/kubectl config  use-context ${kubeContext}"
+                        script.sh "kubectl config  current-context"
+                        script.sh "kubectl config  use-context ${kubeContext}"
                         script.sh "helm init --kube-context ${kubeContext}"
                         script.sh "helm plugin install https://github.com/hypnoglow/helm-s3.git"
                         script.sh "helm repo add ${helmRepo} ${helmRepoURL}"
