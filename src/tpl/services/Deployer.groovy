6f2c33495f39843e5f08af0f5a3652d6310a7dda
yorammi <yorammi@tikalk.com>
2018-11-21 16:43:26 +0200

    convert shell to on-the-fly script

--- src/tpl/services/Deployer.groovy
+++ src/tpl/services/Deployer.groovy
@@ -140,8 +140,8 @@ class Deployer implements Serializable {
     void helmDeploy() {
         script.dir("${script.env.WORKSPACE}/ghost") {
             script.sh "kubectl config use-context ${kubeContext}"
+            script.sh "helm install ghost ghoster"
             def scriptContent = '''
-                helm install ghost ghoster
                 export APP_HOST=$(kubectl get svc --namespace default ghoster --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
                 export APP_PASSWORD=$(kubectl get secret --namespace default ghoster -o jsonpath="{.data.ghost-password}" | base64 --decode)
                 export APP_DATABASE_PASSWORD=$(kubectl get secret --namespace default ghoster-mariadb -o jsonpath="{.data.mariadb-password}" | base64 --decode)
