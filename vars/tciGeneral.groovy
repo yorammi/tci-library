9399787ae396ec6d66921c8cbf89be91fd60f43a
yorammi <yorammi@tikalk.com>
2018-12-22 23:33:02 +0200

    #4 debug echo

--- vars/tciGeneral.groovy
+++ vars/tciGeneral.groovy
@@ -3,14 +3,16 @@
 @NonCPS
 def echo(String text) {
 
-    def out
-    def config = new HashMap()
-    def bindings = getBinding()
-    config.putAll(bindings.getVariables())
-    out = config['out']
-
-    def now = new Date()
-    def tstamp = now.format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('UTC'))
-
-    echo "["+tstamp+"] " + text;
+//    def out
+//    def config = new HashMap()
+//    def bindings = getBinding()
+//    config.putAll(bindings.getVariables())
+//    out = config['out']
+//
+//    def now = new Date()
+//    def tstamp = now.format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('UTC'))
+//
+//    echo "["+tstamp+"] " + text;
+    echo text;
 }
+
