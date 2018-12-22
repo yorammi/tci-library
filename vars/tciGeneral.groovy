b5a846d75573d001cfe8b2a5333ff76187ca8bc5
yorammi <yorammi@tikalk.com>
2018-12-22 23:38:10 +0200

    #4 rename echo to timedEcho

--- vars/tciGeneral.groovy
+++ vars/tciGeneral.groovy
@@ -1,17 +1,17 @@
 #!/usr/bin/groovy
 
-def echo(String text) {
+@NonCPS
+def timedEcho(String text) {
 
-//    def out
-//    def config = new HashMap()
-//    def bindings = getBinding()
-//    config.putAll(bindings.getVariables())
-//    out = config['out']
-//
-//    def now = new Date()
-//    def tstamp = now.format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('UTC'))
-//
-//    echo "["+tstamp+"] " + text;
-    echo text;
+    def out
+    def config = new HashMap()
+    def bindings = getBinding()
+    config.putAll(bindings.getVariables())
+    out = config['out']
+
+    def now = new Date()
+    def tstamp = now.format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('UTC'))
+
+    echo "["+tstamp+"] " + text;
 }
 
