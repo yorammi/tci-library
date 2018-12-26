968adb9f2dabc40c4870e3cc55ea7707f1d54b10
yorammi <yorammi@tikalk.com>
2018-12-27 00:31:19 +0200

    #4 add build user class

--- vars/tplGetBuildUserEmailAddress.groovy
+++ vars/tplGetBuildUserEmailAddress.groovy
@@ -2,23 +2,6 @@ import hudson.tasks.Mailer
 import hudson.model.User
 
 def call() {
-    try{
-        def userId = tplGetBuildUserId()
-        if("" != userId)
-        {
-            User user = User.get(userId)
-            def userMail = user.getProperty(Mailer.UserProperty.class)
-            def userEmailAddress = userMail.getAddress()
-            return userEmailAddress
-        }
-        else
-        {
-            return ""
-        }
-    }
-    catch (Exception error)
-    {
-        println(error.getMessage())
-        return ""
-    }
+    tciGeneral.deprecatedMessage("tplGetBuildUserEmailAddress","tciBuildUser.getBuildUserEmail")
+    return tciBuildUser.getBuildUserEmail()
 }
--- vars/tplGetBuildUserId.groovy
+++ vars/tplGetBuildUserId.groovy
@@ -1,6 +1,4 @@
 def call() {
-    def job = Jenkins.getInstance().getItemByFullName(env.JOB_BASE_NAME, Job.class)
-    def build = job.getBuildByNumber(env.BUILD_ID as int)
-    def userId = build.getCause(Cause.UserIdCause).getUserId()
-    return userId
+    tciGeneral.deprecatedMessage("tplGetBuildUserId","tciBuildUser.getBuildUserID")
+    return tciBuildUser.getBuildUserID()
 }
