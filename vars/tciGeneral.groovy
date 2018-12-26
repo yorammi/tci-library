e5e62556e26f6e036cbd4c2e29e6fea5e65d5ee6
yorammi <yorammi@tikalk.com>
2018-12-26 15:48:50 +0200

    #4 add more general TCI steps

--- vars/tciGeneral.groovy
+++ vars/tciGeneral.groovy
@@ -41,3 +41,19 @@ def findStringInBuildLog(String findText) {
     }
     return false
 }
+
+def clearWorkspaceAsRoot() {
+
+    try
+    {
+        docker.image("library/ubuntu").inside('-u root')
+                {
+                    sh ("find ${WORKSPACE} -mindepth 1 -delete > /dev/null | true")
+                    sh ("chmod -R 777 ${WORKSPACE} > /dev/null | true")
+                }
+    }
+    catch(error)
+    {
+
+    }
+}
