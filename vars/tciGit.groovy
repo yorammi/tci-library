123ade7ce6c27976f3395e4951d0c3e756e693cc
yorammi <yorammi@tikalk.com>
2018-12-27 17:26:08 +0200

    #4 Add tciGit.groovy

--- vars/tciGit.groovy
+++ vars/tciGit.groovy
@@ -1,36 +1,42 @@
 #!/usr/bin/groovy
 
-//def checkout(String repositoryUrl, String branch, String credentialsId, String relativPath=".") {
 def checkout(Map config) {
-    if (config == null) {
-        config = [:]
-    }
-    if (config.url == null || config.url == "") {
-        echo "[ERROR] repository URL must be provided!"
-        currentBuild.result = "FAILURE"
-    }
-    if (config.branch == null) {
-        config.branch = "master"
-    }
-    if (config.credentialsId == null) {
-        if (env.TCI_MASTER_DEFAULT_GIT_CREDENTIAL) {
-            config.credentialsId = env.TCI_MASTER_DEFAULT_GIT_CREDENTIAL
+    try {
+        if (config == null) {
+            config = [:]
         }
-        else {
-            config.credentialsId = "gitsshkey"
+        echo "config.url=${config.url }"
+        if (config.url == null || config.url == "") {
+            echo "[ERROR] repository URL must be provided!"
+            currentBuild.result = "FAILURE"
+            return
         }
-    }
-    if (config.dir == null) {
-        config.dir = "."
-    }
+        if (config.branch == null) {
+            config.branch = "master"
+        }
+        if (config.credentialsId == null) {
+            if (env.TCI_MASTER_DEFAULT_GIT_CREDENTIAL) {
+                config.credentialsId = env.TCI_MASTER_DEFAULT_GIT_CREDENTIAL
+            }
+            else {
+                config.credentialsId = "gitsshkey"
+            }
+        }
+        if (config.dir == null) {
+            config.dir = "."
+        }
 
-    dir(config.dir) {
-        timeout(time: 5, unit: 'MINUTES') {
-            checkout([
-                $class: 'GitSCM', branches: [[name: config.branch]],
-                userRemoteConfigs: [[url: config.url ,credentialsId:config.credentialsId]]
-            ])
+        dir(config.dir) {
+            timeout(time: 5, unit: 'MINUTES') {
+                checkout([
+                        $class: 'GitSCM', branches: [[name: config.branch]],
+                        userRemoteConfigs: [[url: config.url ,credentialsId:config.credentialsId]]
+                ])
+            }
         }
+    }
+    catch (error) {
+        echo "[ERROR] ${error}"
     }
 }
 
