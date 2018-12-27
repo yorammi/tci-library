38a9dbc42e3cf75d4a15075205e53223425ca7d7
yorammi <yorammi@tikalk.com>
2018-12-27 18:13:16 +0200

    #4 Add tciGit.groovy

--- vars/tciGit.groovy
+++ vars/tciGit.groovy
@@ -2,10 +2,9 @@
 
 def checkout(Map config) {
     try {
-//        if (!config) {
-//            config = [:]
-//        }
-        echo "[DEBUG] config.url=${config.url}"
+        if (!config) {
+            config = [:]
+        }
         if (!config.url || config.url == "") {
             echo "[ERROR] repository URL must be provided!"
             currentBuild.result = "FAILURE"
