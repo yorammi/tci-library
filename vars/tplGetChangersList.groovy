8d55707e786a70edcebaa14a04cdb67ccbf45e29
yorammi <yorammi@tikalk.com>
2019-01-01 10:33:59 +0200

    add tciGit.getChangersList and tciGit.getChangesList steps [Branch] feature/#4-adjust-to-tci

--- vars/tplGetChangersList.groovy
+++ vars/tplGetChangersList.groovy
@@ -1,31 +1,4 @@
 def call() {
-    try
-    {
-        List<String> changersString = []
-        def changeLogSets = currentBuild.changeSets
-
-        def remoteConfigs = scm.getUserRemoteConfigs()
-        def scmUrl = remoteConfigs[0].getUrl()
-        repoUrl = scmUrl.take(scmUrl.size()-4)
-        scmCommitPrefix = repoUrl+"/commit/"
-        repoName = repoUrl.drop(repoUrl.lastIndexOf("/")+1)
-        if(changeLogSets.size()>0)
-        {
-            for (int i = 0; i < changeLogSets.size(); i++) {
-                def entries = changeLogSets[i].items
-                for (int j = 0; j < entries.size(); j++) {
-                    def entry = entries[j]
-                    def emailAddress = entry.authorEmail
-                    def slackUser = emailAddress.substring(0, emailAddress.lastIndexOf("@"))
-                    changersString.add(slackUser)
-                }
-            }
-        }
-        return changersString.unique()
-    }
-    catch (Exception error)
-    {
-        println(error.getMessage())
-        return null
-    }
+    tciGeneral.deprecatedMessage("tplGetChangersList","tciGit.getChangersList")
+    ttciGit.getChangersList()
 }
