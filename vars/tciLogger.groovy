a01ab32924930eba36f25125aab56b7b79faefb7
yorammi <yorammi@tikalk.com>
2019-01-01 16:40:18 +0200

    add prefix to logger methods [Branch] feature/#4-adjust-to-tci

--- vars/tciLogger.groovy
+++ vars/tciLogger.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/groovy
 
 void info(String message) {
-    tciGeneral.timedEcho(message)
+    tciGeneral.timedEcho("[INFO] "+message)
 }
 
 void debug(String message) {
     if (params.debugLogging || "${env.TCI_DEBUG_MODE}" == "true") {
-        tciGeneral.timedEcho(message)
+        tciGeneral.timedEcho("[DEBUG] "+message)
     }
 }
