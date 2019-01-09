59d553daa00ecc5382538a02141a8cee1b8ff37e
yorammi <yorammi@tikalk.com>
2019-01-09 08:31:07 +0200

    tci-master CI [Branch] feature/#4-adjust-to-tci

--- vars/tciTciMasterCI.groovy
+++ vars/tciTciMasterCI.groovy
@@ -24,9 +24,40 @@ def call() {
                 steps {
                     script {
                         currentBuild.displayName += " [TYPE] ${params.TYPE} [VERSION] ${params.VERSION}"
+                        sh '''
+                            tciGit.gitCheckout(repoUrl: "git@github.com:TikalCI/tci-bloody-jenkins.git",
+                                    dir: 'tci-bloody-jenkins',
+                                    branch: 'master')
+                            tciGit.gitCheckout(repoUrl: "git@github.com:TikalCI/tci-master.git",
+                                    dir: 'tci-master',
+                                    branch: 'master')
+                            cp tci-bloody-jenkins/plugins.txt plguins.txt
+                            cat tci-master/src/resources/tci/plugins-${TYPE}.txt >> plugins.txt
+                            cat plugins.txt | sort > tci-bloody-jenkins/plugins.txt
+                        '''
                     }
                 }
             }
+            stage("Docker build") {
+                steps {
+                    script {
+                        sh '''
+                            docker build -t tikalci/tci-master-${TYPE}:latest tci-bloody-jenkins
+                            docker tag tikalci/tci-master-${TYPE}:latest tikalci/tci-master-${TYPE}:${VERSIOPN}
+                        '''
+                    }
+                }
+            }
+//            stage("Publish") {
+//                steps {
+//                    script {
+//                        sh '''
+//                            docker push tikalci/tci-master-${TYPE}:latest
+//                            docker push tikalci/tci-master-${TYPE}:${VERSIOPN}
+//                        '''
+//                    }
+//                }
+//            }
         }
     }
 }
