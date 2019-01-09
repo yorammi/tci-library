4f2a41fbb6698dbbc20e27f58d9f93c8c38e4b5b
yorammi <yorammi@tikalk.com>
2019-01-09 06:52:06 +0200

    Add template pipeline [Branch] master

---
+++ vars/tciPipelines/templatePipeline.groovy
@@ -0,0 +1,20 @@
+//@Library ("tci-library") _
+
+pipeline {
+    agent { label 'tci-jnlp-node' }
+    options {
+        timestamps()
+        buildDiscarder(logRotator(numToKeepStr: '30'))
+        ansiColor('xterm')
+    }
+    stages {
+        stage("Step") {
+            steps {
+                script {
+                    def buildUserDisplayName = tciBuildUser.getBuildUserDisplayName()
+                    currentBuild.displayName += " - activated by: ${buildUserDisplayName}"
+                }
+            }
+        }
+    }
+}
