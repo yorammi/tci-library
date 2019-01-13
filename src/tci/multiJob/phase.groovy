bd5c1077af89c1bc21139b4a3780eca968193bdd
yorammi <yorammi@tikalk.com>
2019-01-13 14:07:00 +0200

    Add all tests [Branch] feature/#2-tplMultiJob

--- src/tci/multiJob/phase.groovy
+++ src/tci/multiJob/phase.groovy
@@ -6,24 +6,28 @@ class phase implements Serializable {
 
     def script
     def jobs = []
+    def job1
+    def job2
 
     phase(script) {
         this.script = script
     }
 
     void addJob(Map config) {
-        echo config.jobName
         if (config.jobName == null) {
             throw ("[ERROR] You must provide a jobName!")
         }
+
+        def job = phaseJob.newInstance(script,config.jobName)
+        jobs << job
     }
 
     void run() {
         script.timestamps() {
             def parallelBlocks = [:]
             def counter=1
+
             jobs.each { item ->
-                script.echo item.jobName
                 def currentIndex = counter
                 parallelBlocks[item.jobName+':'+currentIndex] = {
                     script.stage(item.jobName+':'+currentIndex) {
