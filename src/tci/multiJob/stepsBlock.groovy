9be23e846eede310920547e4f5e4469186da6d5c
yorammi <yorammi@tikalk.com>
2019-01-13 04:10:02 +0200

    Add phase and stepsBlock classes [Branch] feature/#2-tplMultiJob

--- src/tci/multiJob/stepsBlock.groovy
+++ src/tci/multiJob/stepsBlock.groovy
@@ -3,17 +3,19 @@ package tci.multiJob;
 class stepsBlock implements Serializable {
 
     def script
-    String[] steps = [:]
+    def step1
+    def step2
+    def step3
 
     stepsBlock(script) {
         this.script = script
 
         script.echo "before echo 1"
-        steps[0] = "echo test1"
+        step1 = "echo test1"
         script.echo "before echo 2"
-        steps[1] = "echo test2"
+        step2 = "echo test2"
         script.echo "before echo 3"
-        steps[2] = "echo test3"
+        step3 = "echo test3"
     }
 
 }
