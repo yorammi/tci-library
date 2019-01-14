4d4d1a54c9f065cf67ffb2f9e367be910e1475f8
yorammi <yorammi@tikalk.com>
2019-01-14 07:44:42 +0200

    parallelPhase [Branch] feature/#2-tplMultiJob

---
+++ src/tci/pipeline/parallelPhase.groovy
@@ -0,0 +1,69 @@
+package tci.pipeline;
+
+import tci.pipeline.parallelPhaseSubJob
+
+class parallelPhase implements Serializable {
+
+    class parallelPhaseSubJob implements Serializable {
+
+        def script
+        def jobName
+        boolean propagate
+
+        parallelPhaseSubJob(script, String jobName,boolean propagate ) {
+            this.script = script
+            this.jobName = jobName
+            this.propagate = propagate
+
+        }
+    }
+
+    def script
+    def jobs = []
+    boolean failFast = false
+
+    parallelPhase(script, boolean failFast = false) {
+        this.script = script
+        this.failFast = failFast
+    }
+
+    void addSubJob(String jobName, Map config) {
+        if (config.propagate == null) {
+            config.propagate = false
+        }
+        if (config.wait == null) {
+            config.wait = false
+        }
+
+        def job = parallelPhaseSubJob.newInstance(script, jobName, config.propagate, config.wait)
+        jobs << job
+    }
+
+    void run() {
+        script.timestamps() {
+            def parallelBlocks = [:]
+
+            def counter=1
+            jobs.each { item ->
+                script.stage(item.jobName) {
+                    parallelBlocks[item.jobName] = {
+                        script.build (job: item.jobName, propagate: item.propagate , wait: item.wait)
+                    }
+                }
+            }
+
+            tciGeneral.tciPhase {
+                try {
+                    parallelBlocks.failFast = failFast
+                    script.parallel parallelBlocks
+                }
+                catch (error)
+                {
+                    throw ("[ERROR] TCI parallel phase failed!")
+                }
+            }
+        }
+    }
+
+}
+
--- vars/tciGeneral.groovy
+++ vars/tciGeneral.groovy
@@ -123,6 +123,23 @@ def saveBuildLogFile() {
     {}
 }
 
+@NonCPS
+def tciPhase(Closure body, Map config) {
+    if(config == null) {
+        config = [:]
+    }
+    if( config.name == null)
+    {
+        config.name = "TCI"
+    }
+    tciLogger.info(config.name +" phase started")
+    def timeStart = new Date()
+    body()
+    def timeStop = new Date()
+    def duration = TimeCategory.minus(timeStop, timeStart)
+    tciLogger.info(config.name +" phase ended. Phase duration: ${duration}")
+}
+
 
 
 
