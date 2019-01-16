fc04f1c960d1b2397a368f26833e78d99f03b5e1
yorammi <yorammi@tikalk.com>
2019-01-16 13:31:03 +0200

    tciDefaults

--- vars/tciDefaults.groovy
+++ vars/tciDefaults.groovy
@@ -8,10 +8,13 @@ def initDefaults() {
 
 def isDebugMode() {
     if("${env.TCI_DEBUG_MODE}" != "true" && "${env.TCI_DEBUG_MODE}" != "false") {
+        echo "1"
         initDefaults()
     }
     if("${env.TCI_DEBUG_MODE}" == "true") {
+        echo "2"
         return true
     }
+    echo "3"
     return false
 }
