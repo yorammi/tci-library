c88fad753118892e875b89c62cf90c5123c7c8aa
yorammi <yorammi@tikalk.com>
2019-01-19 19:31:38 +0200

    Move phase to tciPipeline.groovy and add config

--- vars/tciPipeline.groovy
+++ vars/tciPipeline.groovy
@@ -3,43 +3,46 @@
 import groovy.time.*
 
 def phase(String phaseName, Closure body) {
-//def phase(String phaseName, Map config, Closure body) {
-//    if (config == null) {
-//        config = [:]
-//    }
-//
-//    if (config.failOnError == null) {
-//        config.failOnError = true
-//    }
-//
-//    if (config.enable == null) {
-//        config.enable = true
-//    }
-//
-//    if(!config.enable) {
-//        tciLogger.info(phaseName+ " TCI phase disabled.")
-//        return
-//    }
+def phase(Map config, Closure body) {
+    if (config == null) {
+        config = [:]
+    }
 
-    tciLogger.info(phaseName+ " TCI phase started")
+    if (config.name == null) {
+        config.name = "TCI phase"
+    }
+    if (config.failOnError == null) {
+        config.failOnError = true
+    }
+
+    if (config.enable == null) {
+        config.enable = true
+    }
+
+    if(!config.enable) {
+        tciLogger.info(config.name+ " TCI phase disabled.")
+        return
+    }
+
+    tciLogger.info(config.name+ " TCI phase started")
     def timeStart = new Date()
     try {
         body()
     }
     catch (error)
     {
-//        if(config.failOnError) {
-            script.tciLogger.info ("[ERROR] TCI phase failed!")
+        if(config.failOnError) {
+            script.tciLogger.info ("[ERROR] "+config.name+" TCI phase failed!")
             throw error
-//        }
-//        else {
-//            script.tciLogger.info ("[WARNING] TCI phase failed, but phase set to continue on error.")
-//        }
+        }
+        else {
+            script.tciLogger.info ("[WARNING] "+config.name+" TCI phase failed, but phase set to continue on error.")
+        }
     }
     finally {
         def timeStop = new Date()
         def duration = TimeCategory.minus(timeStop, timeStart)
-        tciLogger.info(phaseName+ " TCI phase ended." +phaseName+ " phase duration: ${duration}")
+        tciLogger.info(config.name+ " TCI phase ended." +phaseName+ " phase duration: ${duration}")
     }
 }
 
