c9f14fe004b210d91451c234bcea5f7d5dd897d8
yorammi <yorammi@tikalk.com>
2019-02-01 22:45:40 +0200

    fix parallelPhase issue

--- src/tci/pipeline/parallelPhase.groovy
+++ src/tci/pipeline/parallelPhase.groovy
@@ -63,8 +63,10 @@ class parallelPhase implements Serializable {
         def counter=1
         jobs.each { item ->
             def index = counter
-            parallelBlocks["Run job #"+${counter}+": "+item.jobName] = {
-                script.stage("Run job #"+${index}+": "+item.jobName) {
+            parallelBlocks["Run job: "+item.jobName +" (${index})"] = {
+                script.stage("Run job: "+item.jobName) {
+//            parallelBlocks["Run job #"+${counter}+": "+item.jobName] = {
+//                script.stage("Run job #"+${index}+": "+item.jobName) {
                     def timeStart = new Date()
                     script.tciLogger.info ("Starting job: ${item.jobName}")
                     if( item.parameters != null) {
