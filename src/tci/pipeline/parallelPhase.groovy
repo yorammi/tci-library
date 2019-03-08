83790f0aa34ddcefd72dd5465a149f0d7175b755
yorammi <yorammi@tikalk.com>
2019-03-08 23:52:27 +0200

    add job duration, status and url

--- src/tci/pipeline/parallelPhase.groovy
+++ src/tci/pipeline/parallelPhase.groovy
@@ -257,10 +257,10 @@ class parallelPhase implements Serializable {
 
             }
             jobs.each { item ->
-                echo '[Job] '+item.job
+                echo '[Job] '+item.jobName
 //                echo '[duration] '+item.duration
-                echo '[status] '+item.status
-                echo '[url] '+item.url
+                echo '   [status] '+item.status
+                echo '   [url] '+item.url
             }
         }
     }
