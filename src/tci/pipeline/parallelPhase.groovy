10425469b450093d11f88801b2d1a21d0732bde5
yorammi <yorammi@tikalk.com>
2019-03-09 13:19:01 +0200

    comment remoteSubJob code

--- src/tci/pipeline/parallelPhase.groovy
+++ src/tci/pipeline/parallelPhase.groovy
@@ -204,7 +204,7 @@ class parallelPhase implements Serializable {
                     }
                     def timeStop = new Date()
                     def duration = TimeCategory.minus(timeStop, timeStart)
-                    script.tciLogger.info(" Parallel job '${item.jobName}' ended. Duration: ${duration}")
+                    script.tciLogger.info(" Parallel job '\033[1;93m${item.jobName}\033[0m' ended. Duration: \033[1;93m${duration}\033[0m")
                     if(item.propagate == true) {
                         if(item.status == "FAILURE") {
                             overAllStatus="FAILURE"
@@ -294,7 +294,7 @@ class parallelPhase implements Serializable {
                     }
                     def timeStop = new Date()
                     def duration = TimeCategory.minus(timeStop, timeStart)
-                    script.tciLogger.info(" Parallel steps-sequence '${item.sequenceName}' ended. Duration: ${duration}")
+                    script.tciLogger.info(" Parallel steps-sequence '\033[1;93m${item.sequenceName}\033[0m' ended. Duration: \033[1;93m${duration}\033[0m")
                 }
             }
             counter++
