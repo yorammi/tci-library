c1aeeff712da3f46c26b97f134fee3e5ede65b85
yorammi <yorammi@tikalk.com>
2019-03-26 15:35:54 +0200

    better status

--- src/tci/pipeline/parallelPhase.groovy
+++ src/tci/pipeline/parallelPhase.groovy
@@ -175,7 +175,7 @@ class parallelPhase implements Serializable {
         }
         def timeStop = new Date()
         def duration = TimeCategory.minus(timeStop, timeStart)
-        item.duration = duration
+        item.duration = duration.toString()
         script.echo(" Parallel job '\033[1;94m${item.jobName}\033[0m' ended. Duration: \033[1;94m${duration}\033[0m")
     }
 
