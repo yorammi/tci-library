40a895e443ea2a8d3bda00e09d5e5a1f7212b3f8
yorammi <yorammi@tikalk.com>
2019-04-01 05:20:43 +0300

    change exit and exception behiviors

--- src/tci/pipeline/parallelPhase.groovy
+++ src/tci/pipeline/parallelPhase.groovy
@@ -211,11 +211,10 @@ class parallelPhase implements Serializable {
         def timeStop = new Date()
         def duration = TimeCategory.minus(timeStop, timeStart)
         item.duration = duration.toString()
-        script.echo(" Parallel job '\033[1;94m${item.jobName}\033[0m' ended. Duration: \033[1;94m${duration}\033[0m")
-        script.currentBuild.result=item.status
-        // if(item.status!="SUCCESS") {
-        //     throw new Exception("[Job] "+item.jobName+" [Status]  "+item.status)
-        // }
+        script.echo(" Parallel job '\033[1;94m${item.jobName}\033[0m' (${item.url}) ended with ${item.status} status. Duration: \033[1;94m${duration}\033[0m")
+        if(item.status!="SUCCESS") {
+            throw new Exception()
+        }
     }
 
     def runStepsSequence(def item) {
@@ -239,7 +238,7 @@ class parallelPhase implements Serializable {
         def timeStop = new Date()
         def duration = TimeCategory.minus(timeStop, timeStart)
         item.duration = duration.toString()
-        script.tciLogger.info(" Parallel steps-sequence '\033[1;94m${item.sequenceName}\033[0m' ended. Duration: \033[1;94m${duration}\033[0m")
+        script.tciLogger.info(" Parallel steps-sequence '\033[1;94m${item.sequenceName}\033[0m' ended with ${item.status} status. Duration: \033[1;94m${duration}\033[0m")
     }
 
     void run() {
