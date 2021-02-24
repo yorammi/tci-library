a619685ee8293cc5d4913a454f8ca289c438c5ed
Yoram Michaeli <yoram.michaeli@radcom.com>
2021-02-24 23:19:30 +0200

    [master] add simple pipeline {2021-02-24 21:19}

---
+++ vars/simplePipeline.groovy
@@ -0,0 +1,52 @@
+import com.radcom.rcc.radcomCIJob
+
+def call(Map config) {
+    if (config == null) {
+        config = [:]
+    }
+    if (config.sleep == null)
+    {
+        config.sleep = 0
+    }
+    if (config.quietPeriod == null)
+    {
+        config.quietPeriod = 5
+    }
+    if (config.numToKeepStr == null)
+    {
+        config.numToKeepStr = "5"
+    }
+
+    pipeline {
+        agent { label 'RCCBaseAgent' }
+        options {
+            timestamps()
+            buildDiscarder(logRotator(numToKeepStr: config.numToKeepStr))
+            ansiColor('xterm')
+            skipDefaultCheckout()
+            disableConcurrentBuilds()
+            quietPeriod(config.quietPeriod)
+        }
+        parameters {
+            string( name: 'STRING', defaultValue: '', description: 'simple string')
+            booleanParam ( name: 'BOOLEAN', defaultValue: false, description: 'simple boolean')
+        }
+        stages {
+            stage ("[Pipeline setup]") {
+                steps {
+                    script {
+                            currentBuild.displayName "[STRING] ${STRING} [BOOLEAN] ${BOOLEAN}"
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                script {
+                    sleep (config.sleep)
+                }
+            }
+        }
+    }
+}
